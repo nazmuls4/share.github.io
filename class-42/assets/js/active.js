849868c101bd34f5285c5ce85b395949befc7d57
Nazmul Hasan <nazmuls4@yahoo.com>
2017-07-20 11:45:22 +0600

    class-42
    share
    class-42

---
+++ class-42/assets/js/active.js
@@ -0,0 +1,22 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+(function($) {
+    'use strict'; // use strict to start
+
+    jQuery(document).ready(function($) {
+	   
+        
+    });
+
+	
+
+    jQuery(window).load(function() {
+		
+
+    });
+
+
+
+
+}(jQuery));
